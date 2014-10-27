8c354127df2778e5f6c627f9a2efa8b21d927303
whodrivesthisweek <jake.grant@okstate.edu>
2014-10-27 13:09:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 var main = function() {
-//Get Week Number
-// .addClass('info')
-// .removeClass('info')
-
+  // Get today's date
   var today = new Date();
-  var weekNo = today.getWeek();
-  document.getElementById(weekNo).addClass('info');
-
+  
+  // Driving Schedule
+  var schedule = {
+    'wk42' : ['October 13 2014', 'October 19 2014'],
+    'wk43' : ['October 20 2014', 'October 26 2014'],
+    'wk44' : ['October 27 2014', 'November 2 2014'],
+    'wk45' : ['November 3 2014', 'November 9 2014'],
+    'wk46' : ['November 10 2014', 'November 16 2014'],
+    'wk47' : ['November 17 2014', 'November 23 2014'],
+    'wk48' : ['November 24 2014', 'November 30 2014'],
+    'wk49' : ['December 1 2014', 'December 7 2014'],
+    'wk50' : ['December 8 2014', 'December 14 2014'],
+    'wk51' : ['December 15 2014', 'December 21 2014'],
+    'wk52' : ['December 22 2014', 'December 28 2014'],
+    'wk1' : ['December 29 2014', 'January 4 2015'],
+    'wk2' : ['January 5 2015', 'January 11 2015'],
+  };
+  
+  for (var weekno in schedule) {
+    console.log('checking ' + weekno + ', dates have to be within the ' + schedule[weekno] + 'range');
+    //console.log(Date.parse(schedule[weekno]));
+    if (today >= Date.parse(schedule[weekno][0]) && today <= Date.parse(schedule[weekno][1])) {
+      console.log(weekno + ' is the current week!');
+      document.getElementById(weekno).classList.add('active');
+    };
+  };
 };
 
 $(document).ready(main);
-
-
-//Get week number function
-Date.prototype.getWeek = function() {
-  var onejan = new Date(this.getFullyear(),0,1);
-  return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
-}
-
-
-//Schedule
-var schedule = {
-  'wk1' : [1], 'wk2' : [2], 'wk3' : [3],  'wk4' : [4],  'wk5' : [5],  'wk6' : [6],
-  'wk7' : [7], 'wk8' : [8], 'wk9' : [9], 'wk10' : [10], 'wk11' : [11], 'wk12' : [12],
-  'wk13' : [13], 'wk14' : [14], 'wk15' : [15], 'wk16' : [16], 'wk17' : [17], 'wk18' : [18],
-  'wk19' : [19], 'wk20' : [20], 'wk21' : [21], 'wk22' : [22], 'wk23' : [23], 'wk24' : [24],
-  'wk25' : [25], 'wk26' : [26], 'wk27' : [27], 'wk28' : [28], 'wk29' : [29], 'wk30' : [30],
-  'wk31' : [31], 'wk32' : [32], 'wk33' : [33], 'wk34' : [34], 'wk35' : [35], 'wk36' : [36],
-  'wk37' : [37], 'wk38' : [38], 'wk39' : [39], 'wk40' : [40], 'wk41' : [41], 'wk42' : [42],
-  'wk43' : [43], 'wk44' : [44], 'wk45' : [45], 'wk46' : [46], 'wk47' : [47], 'wk48' : [48],
-  'wk49' : [49], 'wk50' : [50], 'wk51' : [51], 'wk52' : [52], 'wk53' : [53]
-};
