8e844b0cbf0b2fd7e5dacd3028fe5772d5fe6af0
whodrivesthisweek <jake.grant@okstate.edu>
2014-11-17 08:11:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,16 @@ var main = function() {
   
   // Driving Schedule
   var schedule = {
-    'wk42' : ['October 13 2014', 'October 19 2014'],
-    'wk43' : ['October 20 2014', 'October 26 2014'],
-    'wk44' : ['October 27 2014', 'November 3 2014'],
     'wk45' : ['November 3 2014', 'November 9 2014'],
     'wk46' : ['November 10 2014', 'November 16 2014'],
-    'wk47' : ['November 17 2014', 'November 23 2014'],
-    'wk48' : ['November 24 2014', 'November 30 2014'],
-    'wk49' : ['December 1 2014', 'December 7 2014'],
-    'wk50' : ['December 8 2014', 'December 14 2014'],
-    'wk51' : ['December 15 2014', 'December 21 2014'],
-    'wk52' : ['December 22 2014', 'December 28 2014'],
-    'wk1' : ['December 29 2014', 'January 4 2015'],
-    'wk2' : ['January 5 2015', 'January 11 2015'],
+    'wk47' : ['November 17 2014', 'November 24 2014'],
+    'wk48' : ['November 24 2014', 'December 1 2014'],
+    'wk49' : ['December 1 2014', 'December 8 2014'],
+    'wk50' : ['December 8 2014', 'December 15 2014'],
+    'wk51' : ['December 15 2014', 'December 22 2014'],
+    'wk52' : ['December 22 2014', 'December 29 2014'],
+    'wk1' : ['December 29 2014', 'January 5 2015'],
+    'wk2' : ['January 5 2015', 'January 12 2015'],
   };
   
   for (var weekno in schedule) {
