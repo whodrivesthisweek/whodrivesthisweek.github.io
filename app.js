4430c426ef861cbd7d306838005fbb85141e780f
whodrivesthisweek <jake.grant@okstate.edu>
2014-12-22 08:54:24 -0600

    Update app.js

--- app.js
+++ app.js
@@ -51,15 +51,6 @@ $(function() {
   });
 });
 
-function toggleNav() {
-  if ($('#site-wrapper').hasClass('show-nav')) {
-    // Do things on Nav Close
-    $('#site-wrapper').removeClass('show-nav');
-  } else {
-    // Do things on Nav Open
-    $('#site-wrapper').addClass('show-nav');
-  }
-  
-}
+$.material.init()
 
 $(document).ready(main);
