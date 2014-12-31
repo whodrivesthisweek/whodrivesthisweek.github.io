08e71d6422dd3e3bb213573e64caa0f470c96ad8
whodrivesthisweek <jake.grant@okstate.edu>
2014-12-30 23:54:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var main = function() {
   
   // Driving Schedule
   var schedule = {
-    'wk50' : ['December 6 2014', 'December 13 2014'],
-    'wk51' : ['December 13 2014', 'December 20 2014'],
-    'wk52' : ['December 20 2014', 'December 27 2014'],
     'wk1' : ['December 27 2014', 'January 4 2015'],
     'wk2' : ['January 3 2015', 'January 10 2015'],
     'wk3' : ['January 10 2015', 'January 17 2015'],
     'wk4' : ['January 17 2015', 'January 24 2015'],
-    'wk5' : ['January 24 2015', 'January 31 2015']
+    'wk5' : ['January 24 2015', 'January 31 2015'],
+    'wk6' : ['January 31 2015', 'February 7 2015'],
+    'wk7' : ['February 7 2015', 'February 14 2015'],
+    'wk8' : ['February 14 2015', 'February 21 2015']
   };
   
   for (var weekno in schedule) {
@@ -19,7 +19,7 @@ var main = function() {
       //console.log(Date.parse(schedule[weekno]));
       if (today >= Date.parse(schedule[weekno][0]) && today <= Date.parse(schedule[weekno][1])) {
         //console.log(weekno + ' is the current week!');
-        document.getElementById(weekno).classList.add('info');
+        document.getElementById(weekno).classList.add('light-primary-color');
       };
     };
   };
@@ -27,7 +27,6 @@ var main = function() {
 // Load Disqus Comments on click
 $('.show-comments').on('click', function(){
           var disqus_shortname = 'whodrivesthisweek'; // Replace this value with *your* username.
- 
           // ajax request to load the disqus javascript
           $.ajax({
                   type: "GET",
@@ -50,7 +49,5 @@ $(function() {
     toggleNav();
   });
 });
-
-$.material.init()
 
 $(document).ready(main);
