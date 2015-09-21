dbedd6e954312acbbcadc5fd524a1526ae79bad8
whodrivesthisweek <jake.grant@okstate.edu>
2015-09-21 11:21:53 -0500

    Update MainController.js

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -90,49 +90,49 @@ app.controller('MainController', ['$scope', function($scope) {
   ]
   $scope.calendar = [
     {
-      sun: 
-      mon: 
-      tue: 1
-      wed: 2
-      thu: 3
-      fri: 4
-      sat: 5
+      sun: ,
+      mon: ,
+      tue: 1,
+      wed: 2,
+      thu: 3,
+      fri: 4,
+      sat: 5,
     },
     {
-      sun: 6
-      mon: 7
-      tue: 8
-      wed: 9
-      thu: 10
-      fri: 11
-      sat: 12
+      sun: 6,
+      mon: 7,
+      tue: 8,
+      wed: 9,
+      thu: 10,
+      fri: 11,
+      sat: 12,
     },
     {
-      sun: 13
-      mon: 14
-      tue: 15
-      wed: 16
-      thu: 17
-      fri: 18
-      sat: 19
+      sun: 13,
+      mon: 14,
+      tue: 15,
+      wed: 16,
+      thu: 17,
+      fri: 18,
+      sat: 19,
     }
     {
-      sun: 20
-      mon: 21
-      tue: 22
-      wed: 23
-      thu: 24
-      fri: 25
-      sat: 26
+      sun: 20,
+      mon: 21,
+      tue: 22,
+      wed: 23,
+      thu: 24,
+      fri: 25,
+      sat: 26,
     },
     {
-      sun: 27
-      mon: 28
-      tue: 29
-      wed: 30
-      thu: 
-      fri: 
-      sat: 
+      sun: 27,
+      mon: 28,
+      tue: 29,
+      wed: 30,
+      thu: ,
+      fri: ,
+      sat: ,
     }
   ]
 }]);
