04a9f07b6a8cabd275a5335d91a150d95fdc23d3
whodrivesthisweek <jake.grant@okstate.edu>
2016-07-28 09:19:34 -0500

    Update MainController.js

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,46 +2,94 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.schedule = [
     {
-      name: 'CONDOR',
-      day: new Date('2016','5','6')
+      name: 'SNAKE',
+      day: new Date('2016','7','1')
     },
     {
       name: 'GIMLI',
-      day: new Date('2016','5','13')
+      day: new Date('2016','7,'8')
+    },
+    {
+      name: 'SNAKE/CONDOR',
+      day: new Date('2016','7','15')
+    },
+    {
+      name: 'SNAKE/CONDOR',
+      day: new Date('2016','7','22')
+    },
+    {
+      name: 'CONDOR',
+      day: new Date('2016','7','29')
+    },
+    {
+      name: 'GIMLI - H',
+      day: new Date('2016','8','5')
     },
     {
       name: 'SNAKE',
-      day: new Date('2016','5','20')
+      day: new Date('2016','8','12')
     },
     {
       name: 'CONDOR',
-      day: new Date('2016','5','27')
+      day: new Date('2016','8','19')
     },
     {
       name: 'GIMLI',
-      day: new Date('2016','6','4')
+      day: new Date('2016','8','26')
     },
     {
       name: 'SNAKE',
-      day: new Date('2016','6','11')
+      day: new Date('2016','9','3')
     },
     {
       name: 'CONDOR',
-      day: new Date('2016','6','18')
+      day: new Date('2016','9','10')
     },
     {
       name: 'GIMLI',
-      day: new Date('2016','6','25')
+      day: new Date('2016','9','17')
+    },
+    {
+      name: 'SNAKE',
+      day: new Date('2016','9','24')
+    },
+    {
+      name: 'CONDOR',
+      day: new Date('2016','9','31')
+    },
+    {
+      name: 'GIMLI',
+      day: new Date('2016','10','7')
+    },
+    {
+      name: 'SNAKE',
+      day: new Date('2016','10','14')
+    },
+    {
+      name: 'CONDOR - H',
+      day: new Date('2016','10','21')
+    },
+    {
+      name: 'GIMLI',
+      day: new Date('2016','10','28')
+    },
+    {
+      name: 'SNAKE',
+      day: new Date('2016','11','5')
+    },
+    {
+      name: 'CONDOR',
+      day: new Date('2016','11','12')
+    },
+    {
+      name: 'GIMLI - H',
+      day: new Date('2016','11','19')
     }
   ]
   $scope.holidays = [
     {
-      name: 'Independence Day',
-      day: new Date('2016', '6', '4')
-    },
-    {
       name: 'Labor Day',
-      day: new Date('2016', '8', '8')
+      day: new Date('2016', '8', '5')
     },
     {
       name: 'Thanksgiving Day',
