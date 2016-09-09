76d86ea686563f5f167c283b057a39faf081a810
Jake G <jakegrantche@gmail.com>
2016-09-08 23:57:31 -0500

    sliding header

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 var app = angular.module('DriveApp', []);
+
+$(function(){
+  $('#page-header').data('size','big');
+});
+
+$(window).scroll(function(){
+  if($(document).scrollTop() > 0)
+  {
+    if($('#page-header').data('size') == 'big')
+    {
+      $('#page-header').data('size','small');
+      $('#page-header').stop().animate({
+        paddingBottom:'20px'
+      },600);
+    }
+  }
+  else
+  {
+    if($('#page-header').data('size') == 'small')
+    {
+      $('#page-header').data('size','big');
+      $('#page-header').stop().animate({
+        paddingBottom:'100px'
+      },600);
+    }
+  }
+});
